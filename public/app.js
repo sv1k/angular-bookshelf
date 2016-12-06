e3688e22d18141f74a7167ffea397e0b7c7e2b96
Yuriy Stryzhekozin <yuriy_s@Yuriys-MacBook-Pro.local>
2016-12-06 21:01:40 +0200

    firebase auth login

--- public/app.js
+++ public/app.js
@@ -2,8 +2,15 @@
   "use strict";
 
   angular.
-    module("bookApp", []).
-    config(function() {
+    module("bookApp", [
+      "ngRoute",
+      "appLogin"
+    ]).
+    config(AppConfig);
+
+    AppConfig.$inject = ["$routeProvider", "$locationProvider"];
+
+    function AppConfig($routeProvider, $locationProvider) {
       var config = {
         apiKey: "AIzaSyA1JpfN0-lBf_uxO7q74awI0s1ky6AIUuM",
         authDomain: "levelup-db.firebaseapp.com",
@@ -11,6 +18,28 @@
         storageBucket: "levelup-db.appspot.com"
       };
       firebase.initializeApp(config);
-    });
+
+      firebase.auth().onAuthStateChanged(function(user) {
+        if(user) {
+          console.log("loged in");
+        }
+      })
+
+      console.log($routeProvider);
+      $routeProvider.
+        when("/login", {
+          template: "<app-login></app-login>"
+        }).
+        when("/", {
+          template: "<a href='/login'>login</a>",
+          controller: function() {
+
+          }
+        });
+
+      $locationProvider.html5Mode(true);
+
+      
+    }
 
 })();
---
+++ public/js/app-login/components/app-login.component.js
@@ -0,0 +1,73 @@
+;(function() {
+  "use strict";
+
+  angular.
+    module("appLogin").
+    component("appLogin", {
+      template: `
+        <div class="login-form-container">
+          <form action="/" ng-submit="$ctrl.onLogin($event)">
+            <label for="login-email">Email: </label>
+            <input id="login-email" type="email" ng-model="$ctrl.login"/><br>
+            <label for="login-password">Password: </label>
+            <input id="login-password" type="password" ng-model="$ctrl.password"/><br>
+            <button>Login</button>
+          </form>
+          <p ng-show="$ctrl.error.isError">{{$ctrl.error.errorText}}</p>
+        </div>
+      `,
+      controller: AppLoginController
+    });
+
+  AppLoginController.$inject = ["$location", "$scope"];
+  function AppLoginController($location, $scope) {
+    let vm = this;
+
+    vm.login = "";
+    vm.password = "";
+    vm.error = {
+      isError: false,
+      errorText: "",
+      setError(text) {
+        this.isError = true;
+        this.errorText = text;
+      },
+      clearError() {
+        this.isError = false;
+        this.errorText = "";
+      }
+    };
+    vm.onLogin = onLogin;
+
+    let usersRef = firebase.database().ref("Users"),
+        auth = firebase.auth();
+
+    function onLogin(ev) {
+      ev.preventDefault();
+
+      let isValidPass = _validatePass(vm.password);
+
+      if(!isValidPass) return;
+
+      auth.
+        signInWithEmailAndPassword(vm.login, vm.password).
+        then(function() {
+          $location.path("/");
+          $scope.apply();
+        }).catch(function(e) {
+          vm.error.setError(e.message);
+        });
+    }
+
+    function _validatePass(pass) {
+      vm.error.clearError();
+
+      if(pass.trim().length < 6) {
+        vm.error.setError("Password should contain at least 6 characters");
+        return false;
+      }
+
+      return true;
+    }
+  }
+})();
