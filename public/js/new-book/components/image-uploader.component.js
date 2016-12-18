1bf2f13c7dcfc1d40de7944da37f24df66d27bc9
sv1k <htchelp61@gmail.com>
2016-12-18 23:44:35 +0200

    Update image-uploader.component.js

--- public/js/new-book/components/image-uploader.component.js
+++ public/js/new-book/components/image-uploader.component.js
@@ -1,94 +1,75 @@
-;(function() {
-  "use strict";
+;(function () {
+	"use strict";
 
-  angular.
-    module("newBook").
-    component("imageUploader", {
-      template: `
-        <div
-          id="dnd-container"
-          ng-style="{'background': $ctrl.imageUploaded ? 'url(' + $ctrl.imageUrl + ') 50% 50% / contain no-repeat' : '#fff'}">
-        </div>
-        <label for="book-cover-select">Select Cover</label>
-        <input id="book-cover-select" name="book-cover-select" type="file" accept=".png,.jpg,.bmp"/>
-      `,
-      bindings: {
-        createCover: "&"
-      },
-      controller: ImageUploaderController
-    });
+	angular.
+		module("newBook").
+		component("imageUploader", {
+			template: ` 
+				<div
+					id="dnd-container"
+					ng-style="{'background': $ctrl.imageUploaded ? 'url(' + $ctrl.imageUrl + ')50% 50%/ cover no-repeat' : '#fff' }">
+					<i class="fa fa-upload fa-3x" aria-hidden="true"></i>
+					<p>Drag & Drop files <br><br><br>Or</p>
+					</div>
+				<label for="book-cover-select" name="book-cover-select">Browse</label>
+				<input id="book-cover-select" type="file" accept=".png,.jpg"/>
+			`,
+			bindings: {
+				createCover: "&"
+			},
+			controller: ImageUploaderController
+		});
 
-  ImageUploaderController.$inject = ["$scope"];
+		ImageUploaderController.$inject = ["$scope"];
 
-  function ImageUploaderController($scope) {
-    let vm = this;
+		function  ImageUploaderController($scope) {
+			let vm = this;
 
-    vm.uploadFile = uploadFile;
-    vm.selectCover = selectCover;
-    vm.imageUploaded = false;
-    vm.imageUrl = null;
+			vm.uploadFile = uploadFile,
+			vm.selectCover = selectCover;
+			vm.imageUploaded = false;
+			vm.imageUrl = null;
 
-    document.getElementById("book-cover-select").addEventListener("change", selectCover);
-    document.getElementById("dnd-container").addEventListener("dragover", cancelDrag, false);
-    document.getElementById("dnd-container").addEventListener("dragenter", cancelDrag, false);
-    document.getElementById("dnd-container").addEventListener("drop", dropCover, false);
+			document.getElementById("book-cover-select").addEventListener("change", selectCover);
+			document.getElementById("dnd-container").addEventListener("dragover", cancelDrag, false);
+			document.getElementById("dnd-container").addEventListener("dragenter", cancelDrag, false);
+			document.getElementById("dnd-container").addEventListener("drop", dropCover, false);
 
-    function uploadFile() {}
-    function selectCover(ev) {
-      let file = ev.target.files[0];
+			function uploadFile() {}
 
-      renderPreview(file);
+			function selectCover(ev) {
+				let file = ev.target.files[0];
+				renderPreview(file);
+				vm.createCover({ file });
+			}
 
-      vm.createCover({ file });
-    }
+			function cancelDrag(ev) {
+				ev.preventDefault();
+				ev.stopPropagation();
+			}
 
-    function cancelDrag(ev) {
-      ev.preventDefault();
-      ev.stopPropagation();
-    }
 
-    function dropCover(ev) {
-      ev.preventDefault();
-      ev.stopPropagation();
+			function dropCover(ev) {
+				ev.preventDefault();
+				ev.stopPropagation();
 
-      let file = ev.dataTransfer.files[0];
+				let file = ev.dataTransfer.files[0];
+				renderPreview(file);
+			}
 
-      renderPreview(file);
-    }
+			function renderPreview(file) {
+				let reader = new FileReader();
+				reader.addEventListener("load", renderImage)
 
-    function renderPreview(file) {
-      let reader = new FileReader();
-      reader.addEventListener("load", renderImage);
+				function renderImage() {
+					vm.imageUrl = reader.result;
+					vm.imageUploaded = true;
+					document.getElementById("dnd-container").style.fontSize="0px";
+					$scope.$apply();
+				}
 
-      function renderImage() {
-        vm.imageUrl = reader.result;
-        vm.imageUploaded = true;
-        $scope.$apply();
-      }
-
-      reader.readAsDataURL(file);
-      vm.createCover({ file });
-    }
-  }
+				reader.readAsDataURL(file)
+				vm.createCover({ file });
+			}
+		}
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
